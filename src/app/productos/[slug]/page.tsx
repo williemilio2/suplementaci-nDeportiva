62e439eb3f410b08ea5c3cab2366dec10258fc6b
williemilio2 <emilio.gonzalezcanovas@gmail.com>
2025-05-08 18:03:23 +0200

    PORFIN TIO PORFINNN

--- src/app/productos/[slug]/page.tsx
+++ src/app/productos/[slug]/page.tsx
@@ -1,21 +1,22 @@
-import ProductDetailClient from "./product-detail-client";
+import ProductDetailClient from "./product-detail-client"
 import { allProducts }  from '../../../others/listaArchivos'
+//import { PageProps } from 'next';
+import { notFound } from 'next/navigation';  // Asumiendo que usas 'notFound'
 
-
+// Define el tipo Props con el tipo correcto para params
 type Props = {
-  params: {
-    slug: string;
-  };
+  params: Promise<{ slug: string }>;
 };
 
-export default function ProductoPage({ params }: Props) {
-  // NO necesitamos await en params
-  const slug = params.slug;
+// Componente ProductoPage
+export default async function ProductoPage({ params }: Props) {
+  const { slug } = await params;  // Esperar a que 'params' se resuelva
 
+  // Buscar el producto con el slug
   const producto = allProducts.find((p) => p.slug === slug);
 
   if (!producto) {
-    return <div>Producto no encontrado</div>;
+    notFound();
   }
 
   return <ProductDetailClient producto={producto} />;
