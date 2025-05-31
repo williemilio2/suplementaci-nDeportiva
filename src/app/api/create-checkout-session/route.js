ea668431be066d68435c7cbeb9403b747b4ab71e
williemilio2 <emilio.gonzalezcanovas@gmail.com>
2025-05-31 22:51:01 +0200

    el puto ts

--- src/app/api/create-checkout-session/route.js
+++ src/app/api/create-checkout-session/route.js
@@ -1,18 +1,26 @@
+import dotenv from 'dotenv';
+import path from 'path';
 import Stripe from 'stripe';
-import { NextRequest } from 'next/server';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-04-30.basil', // Usa esta si '' da error
-});
 
-export const POST = async (req: NextRequest) => {
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+
+export async function POST(req) {
+  const stripe = new Stripe(STRIPE_SECRET_KEY, {
+    apiVersion: '2025-04-30.basil', // Usa esta si '' da error
+  });
+
   try {
     const body = await req.json();
     const precios = body.precioTotal;
     const nombres = body.items;
     const preciosCent = Math.round(precios * 100);
-    console.log(body)
-    console.log(nombres)
+
+    console.log(body);
+    console.log(nombres);
+
     if (!precios || !nombres) {
       return new Response(JSON.stringify({ error: 'Datos incompletos' }), { status: 400 });
     }
@@ -25,9 +33,9 @@ export const POST = async (req: NextRequest) => {
           price_data: {
             currency: 'eur',
             product_data: {
-              name: nombres.join(', '), // une los nombres de productos
+              name: nombres.join(', '),
             },
-            unit_amount: preciosCent, // total en céntimos
+            unit_amount: preciosCent,
           },
           quantity: 1,
         },
@@ -41,4 +49,4 @@ export const POST = async (req: NextRequest) => {
     console.error('Error creando la sesión:', error);
     return new Response(JSON.stringify({ error: 'Error al crear la sesión' }), { status: 500 });
   }
-};
+}
