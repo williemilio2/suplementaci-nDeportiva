9c78f1adbb3a8399720c8292ac10399c33ae53db
williemilio2 <emilio.gonzalezcanovas@gmail.com>
2025-06-02 21:13:38 +0200

    Actualizacion2.0

--- src/app/productos/[slug]/page.tsx
+++ src/app/productos/[slug]/page.tsx
@@ -3,26 +3,14 @@ import { getAllProducts } from "../../../products/listaArchivos"
 import { notFound } from "next/navigation"
 
 type Props = {
-  params: { slug: string }
-}
-
-// Método para generar todas las rutas estáticas (build time)
-export async function generateStaticParams() {
-  const products = await getAllProducts()
-  return products.map((product) => ({
-    slug: product.slug,
-  }))
+  params: Promise<{ slug: string }>
 }
 
 export default async function ProductoPage({ params }: Props) {
-  const { slug } = params
+  const { slug } = await params
 
   const products = await getAllProducts()
-
-  // Normalizamos para evitar problemas con mayúsculas, codificación, etc.
-  const cleanSlug = decodeURIComponent(slug).toLowerCase()
-
-  const producto = products.find((p) => p.slug.toLowerCase() === cleanSlug)
+  const producto = products.find((p) => p.slug === slug)
 
   if (!producto) {
     notFound()
